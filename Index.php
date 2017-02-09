4cc2f27fdbac677b9f7fe473dbea07f3569478f0
anderhidalgo <2gdaw03@a224p03.arriagainfo.local>
2017-02-09 10:08:50 +0100

    quien me ha abierto la maleta
    quien me ha abierto la maleta

--- Index.php
+++ Index.php
@@ -1,11 +1,18 @@
 <?php
 
 
+<<<<<<< Updated upstream
 echo 'jola';
+=======
+<<<<<<< HEAD
+echo 'andercomentario125';
+=======
+echo 'jola';
+>>>>>>> origin/master
+>>>>>>> Stashed changes
 
 require_once __DIR__."/Vista/Login/Login.php";
 
-
-
+echo 'chiflos'
 ?>
 
